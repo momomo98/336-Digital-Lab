82904463cef9670b7dd06a345720a8495c6bfd7f
sut-zzp <1159328074@qq.com>
2024-09-11 10:40:50 +0800

    doc

--- rtl/AXI2NPU_interface.v
+++ rtl/AXI2NPU_interface.v
@@ -1,4 +1,6 @@
 module AXI2NPU_interface(
+    input  wire                         clk                        ,
+    input  wire                         rst_n                      ,
     //wire addr channel signals
     input  wire        [   3: 0]        acc_awid                   ,
     input  wire        [  31: 0]        acc_awaddr                 ,
@@ -31,7 +33,7 @@ module AXI2NPU_interface(
     input  wire        [   3: 0]        acc_arcache                ,
     input  wire        [   2: 0]        acc_arprot                 ,
     input  wire                         acc_arvalid                ,
-    output wire                         acc_arready                 
+    output wire                         acc_arready                , 
     //read data channel signals
     output wire        [   3: 0]        acc_rid                    ,
     output wire        [  31: 0]        acc_rdata                  ,
@@ -52,24 +54,33 @@ module AXI2NPU_interface(
     output reg        [  31: 0]        npu_wr_data_data           
 );
 
-//支持outstanding为8的AXI传输
-//awlen-突发长度寄存器，这个参数在本npu设计中是固定的
+//support outstanding 8
+//awlen-reg fixed and no useful now
 reg [7:0] awlen_reg; 
-//awsize-总线数据有效位宽，这个参数在npu设计中是固定的
+//awsize-reg fixed and no useful now
 reg [2:0] awsize_reg; 
-//awburst-总线突发传输模式，这个参数在npu设计中固定选中increment
+//awburst-reg fixed to increment
 reg [2:0] awburst_reg; 
+//awport-reg 3'b100 is weight / 3'b000 is data
+reg [2:0] awport_reg;
 
-//用于记录outstanding顺序的fifo信号，由于AXI4没有wid，因此outstanding需按顺序传输，可以用fifo
+//awid fifo signal, will be used to process outstanding
 wire awid_fifo_wr_vld;
 wire [3:0] awid_fifo_wr_data;
-
 wire awid_fifo_rd_vld;
 wire [3:0] awid_fifo_rd_data;
-
 wire awid_fifo_wr_full;
 wire awid_fifo_rd_empty;
 
+//awprot fifo signal, will be used to process outstanding
+wire awprot_fifo_wr_vld;
+wire [2:0] awprot_fifo_wr_data;
+wire awprot_fifo_rd_vld;
+wire [2:0] awprot_fifo_rd_data;
+wire awprot_fifo_wr_full;
+wire awprot_fifo_rd_empty;
+
+//awid fifo
 fifo
 #(
     .fifo_deep(8),
@@ -90,11 +101,38 @@ awid_fifo
     .rd_empty(awid_fifo_rd_empty)
 );
 
-//记录写地址阶段的outstanding信息
+//awprot fifo
+fifo
+#(
+    .fifo_deep(8),
+    .fifo_addr_width(3),
+    .fifo_data_width(3)
+)
+awprot_fifo
+(
+    .clk(clk),
+    .rst_n(rst_n),
+    .wr_vld(awprot_fifo_wr_vld),
+    .wr_data(awprot_fifo_wr_data),
+
+    .rd_vld(awprot_fifo_rd_vld),
+    .rd_data(awprot_fifo_rd_data),
+
+    .wr_full(awprot_fifo_wr_full),
+    .rd_empty(awprot_fifo_rd_empty)
+);
+
+//fifo signal logic control
 assign awid_fifo_wr_vld = acc_awvalid && acc_awready;
 assign awid_fifo_wr_data = (acc_awvalid && acc_awready) ? acc_awid : 4'd0;
+assign awid_fifo_rd_vld = acc_wlast;
 
-//在写地址通道握手成功时，把本次写数据的突发长度赋值给寄存器awlen_reg
+assign awprot_fifo_wr_vld = acc_awvalid && acc_awready;
+assign awprot_fifo_wr_data = (acc_awvalid && acc_awready) ? acc_awprot : 3'd0;
+assign awprot_fifo_rd_vld = acc_wlast;
+
+//******************************************* register define and control *******************************************
+//awlen_reg - fixed value during all of the work time
 always@(posedge clk or negedge rst_n)
 begin
     if(~rst_n)
@@ -105,7 +143,7 @@ begin
         awlen_reg <= awlen_reg;
 end
 
-//在写地址通道握手成功时，把本次写数据的位宽赋值给寄存器awsize_reg
+//awsize_reg - fixed value during all of the work time
 always@(posedge clk or negedge rst_n)
 begin
     if(~rst_n)
@@ -116,7 +154,7 @@ begin
         awsize_reg <= awsize_reg;
 end
 
-//在写地址通道握手成功时，把本次写数据的突发传输类型给寄存器awburst_reg
+//awburst_reg - fixed value during all of the work time
 always@(posedge clk or negedge rst_n)
 begin
     if(~rst_n)
@@ -127,7 +165,19 @@ begin
         awburst_reg <= awburst_reg;
 end
 
-//写地址通道握手信号aw_ready生成逻辑
+//awport_reg - the value will be obtained in awport_fifo
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        awport_reg <= 3'd0;
+    else if(acc_wlast)
+        awport_reg <= awprot_fifo_rd_data;
+    else
+        awport_reg <= awport_reg;
+end
+
+//******************************************* axi interface of waddr channel *******************************************
+//aw_ready control logic
 always@(posedge clk or negedge rst_n)
 begin
     if(~rst_n)
@@ -140,13 +190,136 @@ begin
         acc_awready <= 1'b0;
 end
 
-//写数据通道握手信号w_ready生成逻辑 NPU永远都可以接收数据，但这是在上游或软件层面的基础上设计的
+//******************************************* axi interface of wdata channel *******************************************
+//w_ready control logic
 always@(posedge clk or negedge rst_n)
 begin
     if(~rst_n)
         acc_wready <= 1'b0;
-    else if(acc_wvalid)
+    else if(acc_awvalid && acc_awready)
         acc_wready <= 1'b1;
+    else if(acc_wlast)
+        acc_wready <= 1'b0;
+    else
+        acc_wready <= acc_wready;
 end
 
+//******************************************* axi interface of bresp channel *******************************************
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        acc_bid <= 4'b0000;
+    else if(acc_wlast)
+        acc_bid <= awid_fifo_rd_data;
+    else if(acc_bready && acc_bvalid)
+        acc_bid <= 4'b0000;
+    else
+        acc_bid <= acc_bid;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        acc_bresp <= 2'b00;
+    else if(acc_wlast)
+        acc_bresp <= 2'b00;
+    else if(acc_bready && acc_bvalid)
+        acc_bresp <= 2'b00;
+    else
+        acc_bresp <= acc_bresp;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        acc_bvalid <= 1'b0;
+    else if(acc_wlast)
+        acc_bvalid <= 1'b1;
+    else if(acc_bready && acc_bvalid)
+        acc_bvalid <= 1'b0;
+    else
+        acc_bvalid <= acc_bvalid;
+end
+//******************************************* NPU interface *******************************************
+//NPU control logic of weight interface
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_sop_weight <= 1'b0;
+    else if(acc_awvalid && acc_awready && awport_reg[2])
+        npu_wr_sop_weight <= 1'b1;
+    else
+        npu_wr_sop_weight <= 1'b0;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_eop_weight <= 1'b0;
+    else if(acc_wlast && awport_reg[2])
+        npu_wr_eop_weight <= 1'b1;
+    else
+        npu_wr_eop_weight <= 1'b0;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_vld_weight <= 1'b0;
+    else if(acc_wvalid && acc_wready && awport_reg[2])
+        npu_wr_vld_weight <= 1'b1;
+    else
+        npu_wr_vld_weight <= 1'b0;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_data_weight <= 32'd0;
+    else if(acc_wvalid && acc_wready && awport_reg[2])
+        npu_wr_data_weight <= acc_wdata;
+    else
+        npu_wr_data_weight <= 32'd0;
+end
+
+//NPU control logic of data interface
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_sop_data <= 1'b0;
+    else if(acc_awvalid && acc_awready && (~awport_reg[2]))
+        npu_wr_sop_data <= 1'b1;
+    else
+        npu_wr_sop_data <= 1'b0;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_eop_data <= 1'b0;
+    else if(acc_wlast && (~awport_reg[2]))
+        npu_wr_eop_data <= 1'b1;
+    else
+        npu_wr_eop_data <= 1'b0;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_vld_data <= 1'b0;
+    else if(acc_wvalid && acc_wready && (~awport_reg[2]))
+        npu_wr_vld_data <= 1'b1;
+    else
+        npu_wr_vld_data <= 1'b0;
+end
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(~rst_n)
+        npu_wr_data_data <= 32'd0;
+    else if(acc_wvalid && acc_wready && (~awport_reg[2]))
+        npu_wr_data_data <= acc_wdata;
+    else
+        npu_wr_data_data <= 32'd0;
+end
 endmodule
